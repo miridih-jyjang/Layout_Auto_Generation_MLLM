e9ec008a8816f43b4cf86f8930ad33cf763c3e4d
miridih-jyjang <jyjang@miridih.com>
2024-08-26 11:47:34 +0900

    change init to manage prompt version and minimum bbox, aspect ratio, etc

--- convertHTML/__init__.py
+++ convertHTML/__init__.py
@@ -1,7 +1,7 @@
 from convertHTML.miridih import MiriDihDataset
 
-def get_dataset(name, datapath, split, transform=None, max_bbox_bins=32):
-    if name == "miridih":
-        return MiriDihDataset(datapath,split,max_seq_length=125,transform=transform)
-    
+def get_dataset(name, datapath, split, transform=None, min_size=[0,0], min_aspect_ratio=1e5, canvas_aspect_ratio=10, max_bbox_bins=32):
+    if 'miridih' in name:
+        return MiriDihDataset(datapath,split,max_seq_length=125,transform=transform, min_size=min_size, min_aspect_ratio=min_aspect_ratio, canvas_aspect_ratio=canvas_aspect_ratio)
+
     raise NotImplementedError(name)
