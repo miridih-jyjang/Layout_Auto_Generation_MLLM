e8468eabc05279d2f217d88d5017bdac7658e241
miridih-jyjang <jyjang@miridih.com>
2024-08-26 13:54:32 +0900

    add inference code for prompt v4 dataset

--- miridih_llava/serve/cli_multi_v4.py
+++ miridih_llava/serve/cli_multi_v4.py
@@ -21,6 +21,9 @@ import requests
 from io import BytesIO
 from transformers import TextStreamer
 
+bbox_extract = re.compile(r"'label':\s*'([^']+)',\s*'box':\s*\[([-\d.,\s]*)\],\s*'file_name':\s*'([^']*)'")
+bbox_extract_wo_filename = re.compile(r"'label':\s*'([^']+)',\s*'box':\s*\[([-\d.,\s]*)\]")
+
 CLS2COLOR = {
     # "Miridih": {
     #     "generalsvg": "red", "text": "green", "shapesvg": "orange", "photo": "blue", "frameitem": "yellow",
@@ -65,66 +68,13 @@ def get_json_response(response):
         json_response = None
     return json_response
 
-def stringTojson_v2(s):
-    # Find the SVG content within the <body> tags
-    svg_content = re.search(r'<svg\s+([^>]+)>(.*?)<\/svg>', s, re.DOTALL)
-    
-    if not svg_content:
-        print("No SVG content found")
-        return None
-    
-    # Extract the attributes of the <svg> tag
-    svg_attributes = svg_content.group(1)
-    svg_body = svg_content.group(2)
-    
-    width = float(re.search(r'width=["\'](\d+(?:\.\d+)?)["\']', svg_attributes).group(1))
-    height = float(re.search(r'height=["\'](\d+(?:\.\d+)?)["\']', svg_attributes).group(1))
+def stringTojson_v4(s):
     
     # Find all rect elements and their attributes within the SVG body
-    rects = re.findall(r'<rect\s+([^>]+)\/>', svg_body)
-    
+    rects = bbox_extract_wo_filename.findall(s)
     output = []
     for rect in rects:
-        # Extract the attributes of each rect
-        attributes = re.findall(r'([\w\-]+)=["\']([^"\']+)["\']', rect)
-        rect_dict = {attr[0]: attr[1] for attr in attributes}
-        
-        # Map to the original desired format, using extracted width and height for normalization
-        mapped_dict = {
-            "label": rect_dict.get('data-category', ''),
-            "box": [
-                float(rect_dict.get('x', 0)) / width,  # Normalizing with extracted width
-                float(rect_dict.get('y', 0)) / height,  # Normalizing with extracted height
-                (float(rect_dict.get('x', 0)) + float(rect_dict.get('width', 0))) / width,
-                (float(rect_dict.get('y', 0)) + float(rect_dict.get('height', 0))) / height
-            ]
-        }
-        output.append(mapped_dict)
-    
-    return output
-
-
-
-def stringTojson(s):
-    for i in range(len(s)):
-        if i < len(s) - 1 and s[i:i+2] == "[{":
-            lo = i
-        elif i > 1 and s[i-1:i+1] == "}]":
-            hi = i
-
-    tries, max_tries = 0, 1
-    output = ''
-    while tries < max_tries:
-        try:
-            string = s[lo:hi+1].replace("'", '"')
-            output = json.loads(string)
-            break
-        except json.JSONDecodeError as e:
-            tries += 1
-            print(f"Tried for {tries} times, error parsing JSON: {e}")
-        except UnboundLocalError as e:
-            tries += 1
-            print(f"Tried for {tries} times, error parsing JSON: {e}")
+        output.append({'label': rect[0], 'box': rect[1].split(',')})
     
     return output
 
@@ -142,7 +92,7 @@ def draw_box(img, elems, cls2color):
         image_file = f"data/miridih/images/{template_id:08}/{page_num:03}/{file_name}"
         overlay_img = Image.open(image_file).convert("RGBA")
         # color = cls2color[clss.lower()]
-        left, top, right, bottom = box
+        left, top, right, bottom = float(box[0]), float(box[1]), float(box[2]), float(box[3])
         ele_width, ele_height = W*(right-left), H*(bottom-top)
         overlay_img = overlay_img.resize((max(1, round(ele_width)), max(1,round(ele_height))))
         _, _, _, overlay_img_mask = overlay_img.split()
@@ -172,8 +122,9 @@ def load_image(image_file):
         image = Image.open(image_file).convert('RGB')
     return image
 
-def online_rendering(image_folder, skin_img_file, annotations, i_entry):
+def online_rendering(image_folder, skin_img_file, annotations, i_entry, args):
     rendering_image = Image.open(os.path.join(image_folder, skin_img_file))
+    W, H = rendering_image.size
     merge_image = Image.new("RGBA", rendering_image.size)
     img_width, img_height = rendering_image.size
     for idx, anno in enumerate(annotations):
@@ -183,65 +134,47 @@ def online_rendering(image_folder, skin_img_file, annotations, i_entry):
         page_num = int(page_num)
         image_file = f"{image_folder}/miridih/images/{template_id:08}/{page_num:03}/{ele_img_file}"
         overlay_img = Image.open(image_file).convert("RGBA")
-        ele_width, ele_height = float(anno['width']), float(anno['height'])
-        
-        x_offset, y_offset = float(anno['x']), float(anno['y'])
+        ele_width, ele_height = W*(float(anno['x2'])-float(anno['x1'])), H*(float(anno['y2']) - float(anno['y1']))
+            
+        x_offset, y_offset = W*float(anno['x1']), H*float(anno['y1'])
         overlay_img = overlay_img.resize((max(1, round(ele_width)), max(1,round(ele_height))))
         _, _, _, overlay_img_mask = overlay_img.split()
         rendering_image.paste(overlay_img, (int(x_offset), int(y_offset)), overlay_img_mask)
         merge_image = Image.alpha_composite(merge_image, rendering_image)
         
-    # if len(annotations) > 0 and i_entry < 10:
-    #     temp= anno['file_name'].replace('.png', f'_{idx+1}.png')
-    #     merge_image.save(f'{image_folder}/online_render_skin/{temp}')
-    
-    #     image_file = f"data/miridih/images/{template_id:08}/{page_num:03}/{template_id:08}_{page_num:01}_0.png"
-            
-    #     if not os.path.isfile(f"data/online_render_skin/{template_id:08}_{page_num:01}_gt.png"):
-    #         thumbnail_img = Image.open(image_file)
-    #         thumbnail_img.save(f'data/online_render_skin/{template_id:08}_{page_num:01}_gt.png')
-    
+    if args.debug and len(annotations) > 0 and i_entry < 10:    
+        image_file = f"data/miridih/images/{template_id:08}/{page_num:03}/{template_id:08}_{page_num:01}_0.png"
+        
+        if ((i_entry+1) % 4 == 0) or ((i_entry+1) % 5 == 0): # completion & refinement
+            merge_image.convert('RGB').save(os.path.join('/'.join(args.output_file.split('/')[:-1]), f"{template_id:08}_{page_num:01}_{i_entry}_input.jpg"))
+        if not os.path.isfile(os.path.join('/'.join(args.output_file.split('/')[:-1]), f"{template_id:08}_{page_num:01}_thumbnail.jpg")): # conditional
+            thumbnail_img = Image.open(image_file).convert('RGB')
+            thumbnail_img.save(os.path.join('/'.join(args.output_file.split('/')[:-1]), f"{template_id:08}_{page_num:01}_thumbnail.jpg"))
     return merge_image
 
 def extract_elements(bbox_html):
     # Extract all bounding boxes using the provided pattern
-    bbox_extract = re.compile(
-             r'<rect\s+data-category="([^"]+)"\s*,\s*x="([^"]+)"\s*,\s*y="([^"]+)"\s*,\s*width="([^"]+)"\s*,\s*height="([^"]+)"\s*,\s*file_name="([^"]+)"\s*/>'
-        )
-    svg_content = re.search(r'<svg\s+([^>]+)>(.*?)<\/svg>', bbox_html, re.DOTALL)
-    
-    if not svg_content:
-        print("No SVG content found")
-        return None
-    
-    # Extract the attributes of the <svg> tag
-    svg_attributes = svg_content.group(1)
-    
-    W = float(re.search(r'width=["\'](\d+(?:\.\d+)?)["\']', svg_attributes).group(1))
-    H = float(re.search(r'height=["\'](\d+(?:\.\d+)?)["\']', svg_attributes).group(1))
-    
-
-
     matches = bbox_extract.findall(bbox_html)
     
     # Find and return invalid elements
     invalid_elements, valid_elements = [], []
     for match in matches:
+        box = match[1].split(',')
         category = match[0]
-        x = float(match[1])
-        y = float(match[2])
-        w = float(match[3])
-        h = float(match[4])
-        file_name = match[5]
+        x1 = float(box[0])
+        y1 = float(box[1])
+        x2 = float(box[2])
+        y2 = float(box[3])
+        file_name = match[2]
 
-        if x < 0 or y < 0 or x+w > W or y+h > H:
+        if x1 < 0 or y1 < 0 or x2 > 1 or y2 > 1:
             invalid_elements.append({
                 "file_name": file_name,
-                "data-category": category,
-                "x": x,
-                "y": y,
-                "width": w,
-                "height": h,
+                "label": category,
+                "x1": x1,
+                "y1": y1,
+                "x2": x2,
+                "y2": y2,
             })
         else:
             valid_elements.append(file_name)
@@ -249,62 +182,55 @@ def extract_elements(bbox_html):
     return invalid_elements, valid_elements
     
 def extract_unmasked_elements(bbox_html):
-    bbox_extract = re.compile(
-             r'<rect\s+data-category="([^"]+)"\s*,\s*x="([^"]+)"\s*,\s*y="([^"]+)"\s*,\s*width="([^"]+)"\s*,\s*height="([^"]+)"\s*,\s*file_name="([^"]+)"\s*/>'
-        )
     matches = bbox_extract.findall(bbox_html)
-    file_attributes = [
-            {
-                "file_name": match[5],
-                "data-category": match[0],
-                "x": match[1],
-                "y": match[2],
-                "width": match[3],
-                "height": match[4],
-            }
-            for match in matches if all(attr != "<FILL_" for attr in match[1:5])
-        ]
+    
+    file_attributes = []
+    for match in matches:
+        box = match[1].split(',')
+        if "''" in box:
+            continue
+        file_attributes.append(
+                {
+                    "file_name": match[2],
+                    "label": match[0],
+                    "x1": box[0],
+                    "y1": box[1],
+                    "x2": box[2],
+                    "y2": box[3],
+                }  
+            )
+
     return file_attributes
 
-def modify_rects(input_string):
-    # Function to process and potentially modify the float values
-    def process_float(value):
-        try:
-            # Attempt to convert to float and then to integer
-            return str(int(float(value)))
-        except ValueError:
-            # If the value is not a valid float, return it unchanged
-            return value
-
-    # Find all rect elements with their attributes
-    rects = re.findall(
-        r'(<rect[^>]*data-category="([^"]+)"[^>]*x="([^"]+)"[^>]*y="([^"]+)"[^>]*width="([^"]+)"[^>]*height="([^"]+)"[^>]*/>)',    
-        input_string
-    )
-
-    for full_match, category, x, y, w, h in rects:
-        # Process each float value
-        new_x = process_float(x)
-        new_y = process_float(y)
-        new_w = process_float(w)
-        new_h = process_float(h)
-
-        # Reconstruct the rect element with the modified values
-        new_rect = (
-        f'<rect data-category="{category}" x="{new_x}" y="{new_y}" '
-        f'width="{new_w}" height="{new_h}"/>'
-        )
-
-        # Replace the old rect element with the new one
-        input_string = input_string.replace(full_match, new_rect)
-
-    return input_string
 
 def remove_elements(conversations, filenames):
     def remove_filenames(text):
         for filename_dict in filenames:
-            text = re.sub(r'<rect\s+data-category="[^"]+"\s*,\s*x="[^"]+"\s*,\s*y="[^"]+"\s*,\s*width="[^"]+"\s*,\s*height="[^"]+"\s*,\s*file_name="' + re.escape(filename_dict['file_name']) + r'"\s*/>', '', text)
-            # text = re.sub(r'<rect\s+data-category="[^"]+"\s*,\s*x1="[^"]+"\s*,\s*y1="[^"]+"\s*,\s*x2="[^"]+"\s*,\s*y2="[^"]+"\s*,\s*file_name="' + re.escape(filename_dict['file_name']) + r'"\s*/>', '', text)
+            # Create a regex pattern for the specific file_name
+            # pattern = r"\{'label':\s*'[^']*',\s*'box':\s*\[([^\]]*)\],\s*'file_name':\s*'" + re.escape(filename_dict['file_name']) + r"'\}"
+            # pattern = r"\{'label':\s*'" + re.escape(filename_dict['label']) + r"',\s*'box':\s*\[\s*'[^']*',\s*'[^']*',\s*[\d\.-]+,\s*[\d\.-]+\s*\],\s*'file_name':\s*'" + re.escape(filename_dict['file_name']) + r"'\}"
+            label = filename_dict['label']
+            x1 = filename_dict['x1']
+            y1 = filename_dict['y1']
+            x2 = filename_dict['x2']
+            y2 = filename_dict['y2']
+            file_name = filename_dict['file_name']
+
+            # Create a regex pattern to match JSON objects with the same label and box coordinates
+            pattern = (r"\{'label':\s*'" + re.escape(label) +
+                    r"',\s*'box':\s*\[\s*(" + re.escape(format(x1, '.4f')) + r"|''|\d*\.?),\s*(" +
+                    re.escape(format(y1, '.4f')) + r"|''|\d*\.?),\s*(" +
+                    re.escape(format(x2, '.4f')) + r"|''|\d*\.?),\s*(" +
+                    re.escape(format(y2, '.4f')) + r"|''|\d*\.?)\s*\],\s*'file_name':\s*'" + re.escape(file_name) + r"'\}")
+
+
+
+            # Substitute the pattern with an empty string
+            text = re.sub(pattern, '', text)
+    
+        # Clean up any extra newlines or spaces that might result from the removal
+        text = re.sub(r'\n\s*\n', '\n', text).strip()
+        
         return text
     
     for conversation in conversations:
@@ -312,7 +238,7 @@ def remove_elements(conversations, filenames):
             conversation['value'] = remove_filenames(conversation['value'])
     return conversations
 
-def merge_lists_without_overlap( list1, list2):
+def merge_lists_without_overlap(list1, list2):
     # Use a dictionary to track unique dictionaries by file_name
     merged_dict = {d['file_name']: d for d in list1}
     
@@ -326,7 +252,6 @@ def merge_lists_without_overlap( list1, list2):
 
 def main(args):
     # Model
-    pattern = r', file_name="[^"]*"\s*'
     disable_torch_init()
 
     model_name = get_model_name_from_path(args.model_path)
@@ -356,7 +281,7 @@ def main(args):
     ret, gt = {}, {}
     
     # streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
-    erase_file_name = r', file_name="[^"]*"\s*'
+    erase_file_name = r",\s*'file_name':\s*'[^']*'"
     
     for i_entry, entry in enumerate(tqdm(data, desc="Processing entries")):
         if entry['id'] not in ret:
@@ -382,14 +307,14 @@ def main(args):
                 image_file = f"miridih/images/{template_id:08}/{page_num:03}/{template_id:08}_{page_num:01}_1.png"
             else:
                 image_file = f"miridih/images/{template_id:08}/{page_num:03}/{template_id:08}_{page_num:01}_0.png"
-            image = online_rendering(args.data_path, image_file, merged_filelist, i_entry).convert('RGB')
+            image = online_rendering(args.data_path, image_file, merged_filelist, i_entry, args).convert('RGB')
         else:
             if os.path.isfile(f"data/miridih/images/{template_id:08}/{page_num:03}/{template_id:08}_{page_num:01}_1.png"):
                 image_file = f"miridih/images/{template_id:08}/{page_num:03}/{template_id:08}_{page_num:01}_1.png"
             else:
                 image_file = f"miridih/images/{template_id:08}/{page_num:03}/{template_id:08}_{page_num:01}_0.png"
             if len(invalid_filenames) > 0:
-                image = online_rendering(args.data_path, image_file, invalid_filenames, i_entry).convert('RGB')
+                image = online_rendering(args.data_path, image_file, invalid_filenames, i_entry, args).convert('RGB')
             else:
                 image = Image.open(os.path.join(args.data_path, image_file)).convert('RGB')
 
@@ -405,9 +330,8 @@ def main(args):
         entry['conversations'][0]['value'] = re.sub(erase_file_name, '', entry['conversations'][0]['value'])
         entry['conversations'][1]['value'] = re.sub(erase_file_name, '', entry['conversations'][1]['value'])   
         
-        ## 3. modify float to integer
-        entry['conversations'][0]['value'] = modify_rects(entry['conversations'][0]['value'])
-        entry['conversations'][1]['value'] = modify_rects(entry['conversations'][1]['value'])
+        ## 3. change valid elements
+        entry['conversations'][0]['value'] = re.sub(r'(\d+)\sforeground elements', f'{len(valid_filenames)} foreground elements', entry['conversations'][0]['value'])
 
         inp = '\n'.join(entry["conversations"][0]['value'].split('\n')[1:])
         
@@ -424,7 +348,6 @@ def main(args):
             conv.append_message(conv.roles[0], inp)
         conv.append_message(conv.roles[1], None)
         prompt = conv.get_prompt()
-        prompt = re.sub(pattern, '', prompt)
         
         if "posterllava/posterllava_v0" in args.model_path:
             prompt = replace_keys(prompt, MIRIDIH2QB)
@@ -447,8 +370,8 @@ def main(args):
         
         outputs = tokenizer.decode(output_ids[0, input_ids.shape[1]:]).strip()
         conv.messages[-1][-1] = outputs
-        ret[entry['id']].append(stringTojson_v2(outputs))
-        gt[entry['id']].append(stringTojson_v2(entry['conversations'][1]['value']))
+        ret[entry['id']].append(stringTojson_v4(outputs))
+        gt[entry['id']].append(stringTojson_v4(entry['conversations'][1]['value']))
         if args.debug:
             # Save images with boxes
             if i_entry < 10:
@@ -457,13 +380,15 @@ def main(args):
         
                     # gt_img_file = f"miridih/images/{template_id:08}/{page_num:03}/{template_id:08}_{page_num:01}_0.png"
                     # gt_img = Image.open(os.path.join(args.data_path, gt_img_file)).convert('RGB')
-                if ((i_entry+1) % 4 == 0) or ((i_entry+1) % 5 == 0): # completion & refinement
+                if (i_entry+1) % 5 == 0: # refinement
                     if os.path.isfile(f"data/miridih/images/{template_id:08}/{page_num:03}/{template_id:08}_{page_num:01}_1.png"):
                         image_file = f"miridih/images/{template_id:08}/{page_num:03}/{template_id:08}_{page_num:01}_1.png"
                     else:
                         image_file = f"miridih/images/{template_id:08}/{page_num:03}/{template_id:08}_{page_num:01}_0.png"
                     if len(invalid_filenames) > 0:
-                        image = online_rendering(args.data_path, image_file, invalid_filenames, i_entry).convert('RGB')
+                        args.debug = False
+                        image = online_rendering(args.data_path, image_file, invalid_filenames, i_entry, args).convert('RGB')
+                        args.debug = True
                     else:
                         image = Image.open(os.path.join(args.data_path, image_file)).convert('RGB')
                 if "posterllava/posterllava_v0" in args.model_path:
